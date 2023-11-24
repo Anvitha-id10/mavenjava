dd182aec474337331a60fefaaf23fe2afddf7652
Anvitha-id10 <kondamaduguanvitha10@gmail.com>
2023-11-24 15:06:12 +0530

    message

---
+++ src/main/java/com/kimt/maven_java/App.java
@@ -0,0 +1,13 @@
+package com.kimt.maven_java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )sd
+    {
+        System.out.println( "Hello World!" );
+    }
+}
