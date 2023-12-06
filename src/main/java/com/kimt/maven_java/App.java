9ed4102e89b90b4ef14f9695569e473e738e89d9
Anvitha-id10 <146067758+Anvitha-id10@users.noreply.github.com>
2023-12-06 14:30:54 +0530

    Update App.java

--- src/main/java/com/kimt/maven_java/App.java
+++ src/main/java/com/kimt/maven_java/App.java
@@ -6,7 +6,7 @@ package com.kimt.maven_java;
  */
 public class App 
 {
-    public static void main( String[] args )sd
+    public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
     }
